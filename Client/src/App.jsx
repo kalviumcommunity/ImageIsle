53eef7f1e66d448c6ce53539f637d72737182887
“Rahul” <chaamrthirahul2105@gmail.com>
2024-05-02 16:17:47 +0530

    intialized a frontend/react application

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>IMAGEISLE</h1>
+        <p>ImageIsle is a curated collection of high-quality stock images available for free download. Whether you`re a designer, blogger, marketer, or just someone looking for beautiful images, ImageIsle has something for everyone.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
