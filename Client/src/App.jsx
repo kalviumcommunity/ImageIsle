5be50599a46afab5e329ce8a588e050884957f27
“Rahul” <chaamrthirahul2105@gmail.com>
2024-05-03 21:07:27 +0530

    done styling for navbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,10 @@
-import './App.css'
+import Nav from "./Components/Nav"
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1>IMAGEISLE</h1>
-        <p>ImageIsle is a curated collection of high-quality stock images available for free download. Whether you`re a designer, blogger, marketer, or just someone looking for beautiful images, ImageIsle has something for everyone.</p>
-      </div>
-    </>
+    <div>
+      <Nav/>      
+    </div>
   )
 }
 
